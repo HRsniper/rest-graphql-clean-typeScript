6ae00461ba46df12e60bb4aa13dcf54bbe70a753
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-19 17:59:54 -0400

    return load-last-ranking controller

--- src/presentation/controllers/load-last-ranking.ts
+++ src/presentation/controllers/load-last-ranking.ts
@@ -5,5 +5,19 @@ import { RankingScoreViewModel } from "@/presentation/view-models";
 export class LoadLastRankingController implements Controller {
   constructor(private readonly lastRankingScore: LastRankingScore) {}
 
-  async handle(): Promise<HttpResponse<RankingScoreViewModel[]>> {}
+  async handle(): Promise<HttpResponse<RankingScoreViewModel[]>> {
+    try {
+      const ranking = await this.lastRankingScore.lastScore();
+
+      return {
+        statusCode: 200,
+        data: ranking.map((item) => ({
+          ...item,
+          matchDate: item.matchDate.toISOString(),
+        })),
+      };
+    } catch (error) {
+      return { statusCode: 500, data: error.stack };
+    }
+  }
 }
