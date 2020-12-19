00d0841177209e44c27e0f63f32cb524931f50bf
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-19 16:52:50 -0400

    add ranking-unavailable errors

--- src/data/services/last-ranking-score.ts
+++ src/data/services/last-ranking-score.ts
@@ -1,13 +1,14 @@
 import { RankingScore } from "@/domain/entities";
 import { LastRankingScore } from "@/domain/usecases";
 import { LoadLastRankingRepository } from "@/data/contracts";
+import { RankingUnavailableError } from "@/domain/errors";
 
 export class LastRankingScoreService implements LastRankingScore {
   constructor(private readonly loadLastRankingRepository: LoadLastRankingRepository) {}
 
   async lastScore(): Promise<RankingScore[]> {
     if (new Date().getHours() > 22) {
-      throw new Error("erro");
+      throw new RankingUnavailableError();
     }
 
     return this.loadLastRankingRepository.loadLastRanking();
---
+++ src/domain/errors/index.ts
@@ -0,0 +1,3 @@
+export * from "./ranking-unavailable";
+
+// caso adicionar novas entidades importa de pasta direto
