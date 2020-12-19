8ffc4d36d005e282cb0c3d38eb7b6abcbf266445
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-12-19 16:39:33 -0400

    add ;

--- src/data/contracts/load-last-ranking-repository.ts
+++ src/data/contracts/load-last-ranking-repository.ts
@@ -1,3 +1,3 @@
 export interface LoadLastRankingRepository {
-  loadLastRanking: () => Promise<[]>
+  loadLastRanking: () => Promise<[]>;
 }
--- src/data/services/last-ranking-score.ts
+++ src/data/services/last-ranking-score.ts
@@ -2,7 +2,7 @@ import { RankingScore } from "@/domain/entities";
 import { LastRankingScore } from "@/domain/usecases";
 
 export class LastRankingScoreService implements LastRankingScore {
+  constructor(private readonly loadLastRankingRepository: LoadLastRankingRepository) {}
 
-  async lastScore(): Promise<RankingScore[]> {
-  }
+  async lastScore(): Promise<RankingScore[]> {}
 }
